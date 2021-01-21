19471b5ea4af919cb2f204977b81aa3e7fa8ff41
Andreas Schrell <as@schrell.de>
2021-01-21 22:36:18 +0100

    Navigation eingebaut

--- view.php
+++ view.php
@@ -7,27 +7,74 @@
 
 <script>
 
+var cur = "";
+var prev = [];
+var next = [];
+
 function on(arg) {
-  document.getElementById("list").style.display = "none";
-  document.getElementById("overlay").style.display = "flex";
-  document.getElementById("image").src = arg;
-  window.asScrollHash = arg;
+  setTimeout(() => {
+    document.getElementById("list").style.display = "none";
+    document.getElementById("overlay").style.display = "flex";
+    document.getElementById("image").src = arg;
+    window.asScrollHash = arg;
+    cur = arg;
+  });
 }
+
+function onNext() {
+  if (next[cur] !== "") {
+    on(next[cur]);
+  }
+}
+
+function onPrev() {
+  if (prev[cur] !== "") {
+    on(prev[cur]);
+  }
+}
+
 function off() {
   document.getElementById("overlay").style.display = "none";
   document.getElementById("list").style.display = "block";
   document.getElementById("image").src = '';
   document.getElementById(window.asScrollHash).scrollIntoView();;
+  cur = "";
 }
+
+function setPrev(a, b) {
+  prev[a] = b;
+}
+function setNext(a, b) {
+  next[a] = b;
+}
+
 </script>
 
 <body>
 <h2><?php echo basename(getcwd());?></h2>
-<div id="overlay" class="overlay" onclick="off()"><img id="image" class="large"></div>
+<div id="overlay" class="overlay">
+  <div class="center">
+    <div onclick="onPrev()" class="pfeil pfeilLinks"></div>
+  </div>
+  <img onclick="off()" id="image" class="large">
+  <div class="center">
+    <div onclick="onNext()" class="pfeil pfeilRechts"></div>
+  </div>
+</div>
 <div class="list" width="100%">
     <ul id="list">
 <?php
+      $images = [];
+      $last = "";
+      $i = 0;
       foreach (glob("*.jpg") as $f) {
+        $images[$i++] = $f;
+        echo "<script>setPrev('" . $f . "','" . $last . "');</script>\n";
+        echo "<script>setNext('" . $last . "','" . $f . "');</script>\n";
+        $last = $f;
+      }
+      echo "<script>setNext('" . $last . "','');</script>\n";
+      foreach ($images as $f) {
           echo "<li class=\"image\" id=\"" . $f . "\" onclick=\"on('" . $f . "')\">\n";
           echo "  <img class=\"content-image\" src=\"thumbs/" . $f . "\">\n";
           echo "</li>\n";
